b10023bef22541c918bb5bf5aa20bf61726d0df8
teacher <tibedox@mail.ru>
2023-12-16 16:42:04 +0600

    touch me

--- core/src/com/mygdx/snowfall/Snowfall.java
+++ core/src/com/mygdx/snowfall/Snowfall.java
@@ -1,15 +1,19 @@
 package com.mygdx.snowfall;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 public class Snowfall extends ApplicationAdapter {
 	public static final float SCR_WIDTH = 1280, SCR_HEIGHT = 720;
 
 	SpriteBatch batch;
 	OrthographicCamera camera;
+	Vector3 touch;
 
 	Texture imgSnowflake;
 	Texture ibgBackGround;
@@ -21,6 +25,7 @@ public class Snowfall extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+		touch = new Vector3();
 
 		imgSnowflake = new Texture("snowflake.png");
 		ibgBackGround = new Texture("forest.png");
@@ -28,14 +33,83 @@ public class Snowfall extends ApplicationAdapter {
 		for (int i = 0; i < snowflakes.length; i++) {
 			snowflakes[i] = new Snowflake();
 		}
+		InputProcessor processor = new InputProcessor() {
+			@Override
+			public boolean keyDown(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyUp(int keycode) {
+				return false;
+			}
+
+			@Override
+			public boolean keyTyped(char character) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+				touch.set(screenX, screenY, 0);
+				camera.unproject(touch);
+
+				for (int i = 0; i < snowflakes.length; i++) {
+					if(snowflakes[i].hit(touch.x, touch.y)){
+						snowflakes[i].respawn();
+					}
+				}
+				return false;
+			}
+
+			@Override
+			public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
+				return false;
+			}
+
+			@Override
+			public boolean touchDragged(int screenX, int screenY, int pointer) {
+				return false;
+			}
+
+			@Override
+			public boolean mouseMoved(int screenX, int screenY) {
+				return false;
+			}
+
+			@Override
+			public boolean scrolled(float amountX, float amountY) {
+				return false;
+			}
+		};
+		Gdx.input.setInputProcessor(processor);
 	}
 
 	@Override
 	public void render () {
+		// обработка касаний
+		/*if(Gdx.input.justTouched()){
+			touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touch);
+
+			for (int i = 0; i < snowflakes.length; i++) {
+				if(snowflakes[i].hit(touch.x, touch.y)){
+					snowflakes[i].respawn();
+				}
+			}
+		}*/
+
+		// события игры
 		for (int i = 0; i < snowflakes.length; i++) {
 			snowflakes[i].move();
 		}
 
+		// отрисовка всего
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
